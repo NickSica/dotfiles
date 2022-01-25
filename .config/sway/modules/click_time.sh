c27db9edcb11e5ebe577c8155a7ef40a9b4f5bcd
NickSica <njs82@drexel.edu>
2022-01-25 16:45:47 -0500

    Switch to waybad

---
+++ .config/sway/modules/click_time.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Stolen shamelessly from i3status
+# Show your calendar year with week numbers
+# and press any key to exit
+cal -yw
+read -n 1 -r -s
---
+++ .config/sway/modules/cpu.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+import psutil
+print(psutil.cpu_percent(interval=1), end='')
