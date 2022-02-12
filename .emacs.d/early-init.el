1341acf19993c742b7e30de6d83f2edab65a9847
NickSica <njs82@drexel.edu>
2022-02-11 21:47:07 -0500

    Switch back to use-package

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,1 +1,5 @@
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name  "eln-cache/" "~/.emacs.d/cache/"))))
 (setq package-enable-at-startup nil)
