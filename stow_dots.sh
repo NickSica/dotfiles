78c836eb87f27d42bc0099d009fba63f053d6f63
NickSica <njs82@drexel.edu>
2023-02-21 17:42:11 -0500

    Changes to non-hidden file format and adds GNU Stow(a bit hacky since --dotfiles flag is buggy)

---
+++ stow_dots.sh
@@ -0,0 +1,3 @@
+stow --dotfiles -d ~/dotfiles -t ~/ --ignore dot-emacs.d --ignore dot-config .
+stow --dotfiles -d ~/dotfiles -t ~/.emacs.d dot-emacs.d
+stow --dotfiles -d ~/dotfiles -t ~/.config dot-config
